bbd947c138cdf981816fc4efa4e2126d83e27ced
nilas <nilasschusler@gmail.com>
2023-06-02 14:33:10 +0200

    Update job script so that logs should end up in the logs directory

--- src/s01_e01_train_job_gpu.sh
+++ src/s01_e01_train_job_gpu.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #PBS -A vaccine -W group_list=vaccine
 #PBS -l nodes=1:gpus=1:ppn=4,mem=12GB,walltime=1:00:00
-#PBS -d /home/projects/vaccine/people/nilsch/masters_thesis/logs
+#PBS -d /home/projects/vaccine/people/nilsch/masters_thesis/
+#PBS -e /home/projects/vaccine/people/nilsch/masters_thesis/logs/
+#PBS -o /home/projects/vaccine/people/nilsch/masters_thesis/logs/
 
 ##$1-Config file path
 ##$2-Test partition index
@@ -15,4 +17,4 @@ source /home/projects/vaccine/people/nilsch/mambaforge/etc/profile.d/conda.sh
 conda activate env
 
 ##Run Model
-./../src/s01_train.py -c $1 -t $2 -v $3
+./src/s01_train.py -c $1 -t $2 -v $3

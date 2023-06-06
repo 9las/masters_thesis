0a196618f8377d19acfb768a10974846584ea947
nilas <nilasschusler@gmail.com>
2023-06-06 16:17:47 +0200

    Initialise embedding script and make environment file for bio embeddings

---
+++ src/embed.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import pandas as pd
+import time
+from bio_embeddings.embed import ESM1bEmbedder
+#from bio_embeddings.embed import SeqVecEmbedder
+data = pd.read_csv(filepath_or_buffer = '../data/raw/nettcr_train_swapped_peptide_ls_3_26_peptides_full_tcr_final.csv',
+                   index_col = 0)
+
+embedder = ESM1bEmbedder()
+#embedder = SeqVecEmbedder()
+for i in range(10):
+    start_time = time.time()
+    embedding = embedder.embed(data.TRA_aa[i])
+    print(time.time()-start_time)
+    print(embedding.shape)
+#embedding = embedder.embed("SEQVENCE")

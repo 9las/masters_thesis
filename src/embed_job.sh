e77170e18f2c5144993afcc418f1b4b1f337d4b9
nilsch nilsch <nilsch@g-12-l0002.cm.cluster>
2023-06-09 10:58:21 +0200

    Set HOME environment varibale for embedding job script to avoid memory issues

--- src/embed_job.sh
+++ src/embed_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -A vaccine -W group_list=vaccine
-#PBS -l nodes=1:gpus=1:ppn=4,mem=188GB,walltime=99:00:00
+#PBS -l nodes=1:gpus=1:ppn=4,mem=12GB,walltime=0:15:00
 #PBS -d /home/projects/vaccine/people/nilsch/masters_thesis/src/
 #PBS -e /home/projects/vaccine/people/nilsch/masters_thesis/logs/
 #PBS -o /home/projects/vaccine/people/nilsch/masters_thesis/logs/
@@ -13,7 +13,7 @@ source /home/projects/vaccine/people/nilsch/mambaforge/etc/profile.d/conda.sh
 conda activate env_bio_embeddings
 
 # Run model
-./embed_test.py
+HOME=/home/projects/vaccine/people/nilsch ./embed.py
 
 # Get status
 qstat -f -1 $PBS_JOBID

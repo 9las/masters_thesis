dc7063ee13495d882fa96a2c8ae999d773598fd2
nilas <nilasschusler@gmail.com>
2023-06-21 10:07:57 +0200

    Update project functions

--- src/s01_train.py
+++ src/s01_train.py
@@ -71,8 +71,6 @@ data = pd.read_csv(filepath_or_buffer = os.path.join('../data/raw',
                               'partition',
                               'original_index'])
 
-#data = data.query("peptide == 'GILGFVFTL'")
-
 # Import model
 model = getattr(s98_models, model_name)
 

a16ace9a75752d14b2b57553471825d8e48964ba
nilas <nilasschusler@gmail.com>
2023-08-15 16:46:53 +0200

    Add configs and job scripts for model 19-22

--- src/s02_m18_train_job_parallel_gpu.sh
+++ src/s02_m18_train_job_parallel_gpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -A vaccine -W group_list=vaccine
-#PBS -l nodes=1:gpus=1:ppn=4,mem=188GB,walltime=3:00:00
+#PBS -l nodes=1:gpus=1:ppn=4,mem=188GB,walltime=4:00:00
 #PBS -t 0-19%20
 #PBS -d /home/projects/vaccine/people/nilsch/masters_thesis/src/
 #PBS -e /home/projects/vaccine/people/nilsch/masters_thesis/logs/

22d16a73f9bf4efda5bf1d4d1e51bc803eecde64
nilas <nilasschusler@gmail.com>
2023-09-14 00:47:11 +0200

    Make configs and job scripts for model 42-49

--- src/s13_plot_validation_performance.R
+++ src/s13_plot_validation_performance.R
@@ -4,14 +4,31 @@ library(glue)
 
 # Read data ---------------------------------------------------------------
 
-data <- read_tsv(file = "../data/s12_validation_performance_summary.tsv",
-                 col_types = cols(tcr_normalization_divisor = col_factor()))
+data <- read_tsv(file = "../data/s12_validation_performance_summary.tsv")
 
 data <- data |>
   mutate(test_validation_partition = str_glue("t{test_partition}v{validation_partition}"))
 
 data |>
   filter(model_index %in% c(19, 25, 34)) |>
+  ggplot(mapping = aes(x = tcr_normalization_divisor,
+                       y = auc,
+                       color = test_validation_partition,
+                       group = test_validation_partition)) +
+  geom_line()+
+  geom_point()
+
+data |>
+  filter(model_index %in% c(5, 16, 36, 37 ,38)) |>
+  ggplot(mapping = aes(x = tcr_normalization_divisor,
+                       y = auc,
+                       color = test_validation_partition,
+                       group = test_validation_partition)) +
+  geom_line()+
+  geom_point()
+
+data |>
+  filter(model_index %in% c(17, 18, 39, 40 ,41)) |>
   ggplot(mapping = aes(x = tcr_normalization_divisor,
                        y = auc,
                        color = test_validation_partition,

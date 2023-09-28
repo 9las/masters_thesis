e2eb5901de48d5ec8e85bc22bf7c0696724cb85a
nilas <nilasschusler@gmail.com>
2023-09-29 00:39:38 +0200

    Add configs and training job scripts for model 109-120

--- src/s02_m019_train_job_partial1_gpu.sh
+++ src/s02_m019_train_job_partial1_gpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -A vaccine -W group_list=vaccine
 #PBS -l nodes=1:gpus=1:ppn=4,mem=188GB,walltime=6:00:00
-#PBS -t 0-18
+#PBS -t 0-9
 #PBS -d /home/projects/vaccine/people/nilsch/masters_thesis/src/
 #PBS -e /home/projects/vaccine/people/nilsch/masters_thesis/logs/
 #PBS -o /home/projects/vaccine/people/nilsch/masters_thesis/logs/
@@ -15,8 +15,8 @@ cd $PBS_O_INITDIR
 source /home/projects/vaccine/people/nilsch/mambaforge/etc/profile.d/conda.sh
 conda activate env
 
-t_array=(0 0 0 1 1 1 1 2 2 2 2 3 3 3 3 4 4 4 4)
-v_array=(2 3 4 0 2 3 4 0 1 3 4 0 1 2 4 0 1 2 3)
+t_array=(0 1 1 2 3 3 3 4 4 4)
+v_array=(4 3 4 3 1 2 4 0 1 3)
 
 # Run model
 ./s02_train.py -c $1 -t ${t_array[$PBS_ARRAYID]} -v ${v_array[$PBS_ARRAYID]}
